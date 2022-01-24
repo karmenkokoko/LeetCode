9229031c13da68529242b362fb46bc261be9a5c2
karmenkokoko <895459297@qq.com>
2022-01-24 17:25:29 +0900

    int split

--- acwing/dynamic_p/Int_split.cpp
+++ acwing/dynamic_p/Int_split.cpp
@@ -2,6 +2,22 @@
 // 整数划分
 using namespace std;
 
-const int N = 100010;
+const int N = 100010, mod = 1e9+7;
+int n;
+int f[N];
 
-
+int main()
+{
+    cin >> n;
+    f[0] = 1;
+    // 前i个数恰好拼成j的方案数
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            f[j] = (f[j] + f[j - i]) % mod;
+        }
+    }
+    cout << f[n] << endl;
+    return 0;
+}
